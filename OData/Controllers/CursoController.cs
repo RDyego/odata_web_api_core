3a6690d642b5b714ea69a96a29cc1814deed3ca6
RabeloS <leandro.dif@gmail.com>
2019-05-20 21:03:53 -0300

    Tentando efetuar acesso ao api

---
+++ OData/Controllers/CursoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using OData.Contexto;
+using OData.Modelo;
+using System.Linq;
+
+namespace OData.Controllers
+{
+    [Route("api/curso")]
+    public class CursoController : ODataController
+    {
+
+        private readonly Context _context;
+
+        public CursoController(Context context)
+        {
+            _context = context;
+        }
+
+
+        [EnableQuery]
+        public IQueryable<Curso> Get() => _context.Curso.AsQueryable();
+    }
+}
--- OData/Startup.cs
+++ OData/Startup.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OData.Edm;
 using OData.Contexto;
+using OData.Modelo;
 
 namespace OData
 {
@@ -46,7 +42,27 @@ namespace OData
             }
 
             app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(
+                b =>
+                {
+                    b.MapODataServiceRoute("odata", "odata", GetEdmModel());
+                });
+        }
+
+        public static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+
+            // Habilita funções OData como $filter, $select e etc..
+            builder.EntitySet<Curso>(nameof(Curso))
+                   .EntityType
+                   .Filter()
+                   .Count()
+                   .Expand()
+                   .OrderBy()
+                   .Page()
+                   .Select();
+            return builder.GetEdmModel();
         }
     }
 }
